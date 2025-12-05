d7cf0bd961a751a25da26ce6a7c509541867f2cb
DarrylJansen <dar951@gmail.com>
2025-12-05 07:23:20 +0100

    code cleanup

--- 2025/5/main-part2.js
+++ 2025/5/main-part2.js
@@ -10,7 +10,6 @@ fs.readFile('input.txt', (err, data) => {
 
     const rangesSet = new Set();
     const processedRanges = new Set();
-    const goodSet = new Set();
 
     let processingRanges = true;
 
@@ -30,15 +29,11 @@ fs.readFile('input.txt', (err, data) => {
     for(let [rangeStart, rangeEnd] of rangesSet){
         for([processedMin, processedMax] of processedRanges){
             //check for collision between ranges
-            // console.log('checking:',rangeStart,rangeEnd,processedMin,processedMax);
-            // || (rangeEnd >= processedMin && rangeEnd <= processedMax)
             if(rangeStart >= processedMin && rangeStart <= processedMax){
-                // console.log('collision (start) between:',rangeStart,rangeEnd,processedMin,processedMax);
                 rangeStart = processedMax + 1;
             }
 
             if(rangeEnd >= processedMin && rangeEnd <= processedMax){
-                // console.log('collision (end) between:',rangeStart,rangeEnd,processedMin,processedMax);
                 rangeEnd = processedMin - 1;
             }
 
@@ -46,23 +41,14 @@ fs.readFile('input.txt', (err, data) => {
                 rangesSet.add([processedMax + 1, rangeEnd]);
                 rangeEnd = processedMin -1;
             }
-
-            
         }
 
         if(rangeEnd >= rangeStart){
-            // console.log('adding:',rangeStart,rangeEnd);
             sum += rangeEnd - rangeStart + 1;
             processedRanges.add([rangeStart, rangeEnd]);
         }
     }
 
 
-    // console.log(processedRanges);
     console.log(sum);
-    // console.log(foodSet);
 });
-
-
-//349485103963315 too high
-//345821388687084
